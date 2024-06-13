78fc73326b0981b749d22101763193e26409164b
merwhite11 <meredith.white91@gmail.com>
2024-06-12 17:08:05 -0700

    Working stack and queue

--- StackAndQueue/stack.js
+++ StackAndQueue/stack.js
@@ -1,32 +1,81 @@
-class Stack {
-  constructor() {
-    this.items = []
-  }
-  add(item) {
-    this.items.push(item)
-  }
-  remove() {
-    this.items.pop()
+class ListNode {
+  constructor(val) {
+    this.value = val;
+    this.next = null
   }
 }
 
-class Queue {
-  constructor() {
-    this.items = []
+class Stack {
+  constructor(head = null) {
+    this.head = head;
   }
-  add(item) {
-    this.items.unshift(item)
+  push(item) {
+    if(this.head) {
+      let newHead = new ListNode(item);
+      newHead.next = this.head;
+      this.head = newHead;
+    } else {
+      this.head = item;
+    }
   }
-  remove() {
-    this.items.shift(item)
+  pop() {
+    if(this.head === null) return;
+    if(this.head.next === null) this.head = null;
+    this.head = this.head.next;
   }
 }
+
+
+const myStack = new Stack(1)
+myStack.pop()
+console.log(myStack)
+myStack.push(2)
+myStack.push(3)
+myStack.push(4)
+myStack.push(5)
+myStack.push(6)
+myStack.pop()
+console.log(myStack)
+
+
+class Queue {
+  constructor(head = null) {
+    this.head = head
+  }
+  enqueue(item) {
+    let newHead = new ListNode(item);
+    newHead.next = this.head;
+    this.head = newHead;
+  }
+  dequeue() {
+    if(this.head === null) return;
+
+    if(this.head.next === null) {
+      this.head = null;
+      return;
+    }
+    let curr = this.head;
+    while(curr.next.next) {
+      curr = curr.next
+    }
+    curr.next = null;
+  }
+}
+
+const queue = new Queue(1)
+queue.enqueue(2)
+queue.enqueue(3)
+queue.enqueue(4)
+queue.enqueue(5)
+console.log(queue)
+queue.dequeue()
+// console.log(queue)
 
 class StackQueue {
   constructor() {
     let store = new Queue()
   }
-  //rewrite push
+  //rewrite enqueue
   //use queue.add then reverse array?
 
   //rewrite remove
